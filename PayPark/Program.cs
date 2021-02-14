2c7ae560a2df096ef277c515b0abf755cb966bb0
Bogo Bogo <bogdan_boy23@yahoo.com>
2021-02-14 11:40:08 +0200

    Added menu validation

--- PayPark/Program.cs
+++ PayPark/Program.cs
@@ -12,37 +12,48 @@ namespace PayPark
         {
 
             IParkingServices _parkingServices = new ParkingServices();
-            int choice = 0;
-            do
+            int choice;
+            string userInput;
+            while(true)
             {
+
                 Menu.DisplayTextMenu();
-                choice = Int32.Parse(Console.ReadLine());
-                switch (choice)
+                userInput = Console.ReadLine();
+                bool validUserInput = int.TryParse(userInput, out choice);
+                if (validUserInput)
                 {
-                    case 1:
-                        CarMessages.EnterNumberOfVehicle();
-                        string plateNumber = Console.ReadLine();
-                        _parkingServices.AddCar(plateNumber);
-                        break;
-                    case 2:
-                        _parkingServices.ViewParkedCar();
-                        break;
-                    case 3:
-                        CarMessages.EnterNumberOfVehicle();
-                        plateNumber = Console.ReadLine();
-                        _parkingServices.UnParkCar(plateNumber);
-                        break;
-                    case 4:
-                        _parkingServices.GetFreeParkingLot();
-                        break;
-                    case 5:
-                        _parkingServices.DisplayParkedCars();
-                        break;
-                    default:
-                        Console.Write("Application will be closed\n");
-                        break;
+                  
+                    switch (choice)
+                    {
+                        case 1:
+                            CarMessages.EnterNumberOfVehicle();
+                            string plateNumber = Console.ReadLine();
+                            _parkingServices.AddCar(plateNumber);
+                            break;
+                        case 2:
+                            _parkingServices.ViewParkedCar();
+                            break;
+                        case 3:
+                            CarMessages.EnterNumberOfVehicle();
+                            plateNumber = Console.ReadLine();
+                            _parkingServices.UnParkCar(plateNumber);
+                            break;
+                        case 4:
+                            _parkingServices.GetFreeParkingLot();
+                            break;
+                        case 5:
+                            _parkingServices.DisplayParkedCars();
+                            break;
+                        default:
+                            Console.WriteLine("Enter a valid number between 1 and 5");
+                            return;
+                    }
                 }
-            } while (choice != 0);
+                else
+                {
+                    Console.WriteLine("Please enter a number from list menu not a string");
+                }
+            } 
         }
 
     }
